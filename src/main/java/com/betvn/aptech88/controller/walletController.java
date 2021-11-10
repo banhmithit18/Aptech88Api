39bd756eef4dbc60a25760eb454268839cdc72df
Phuoc <phuocth@kimei.vn>
2021-11-10 11:05:46 +0700

    Update And Add several feature

--- src/main/java/com/betvn/aptech88/controller/walletController.java
+++ src/main/java/com/betvn/aptech88/controller/walletController.java
@@ -2,12 +2,16 @@ package com.betvn.aptech88.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.betvn.aptech88.model.wallet;
@@ -15,6 +19,7 @@ import com.betvn.aptech88.repository.walletRepository;
 
 import ultis.mapping;
 
+@CrossOrigin(origins = "http://localhost:8080/")
 @RestController
 public class walletController {
 	@Autowired walletRepository wallets;
@@ -39,5 +44,13 @@ public class walletController {
 		{
 			return ResponseEntity.status(404).body("Wallet not found");		
 		}
+	}
+	//get wallet by account id
+	@RequestMapping ( value = "/wallet/getAmount")
+	public @ResponseBody double find (HttpServletRequest request)
+	{
+		int id = Integer.parseInt(request.getParameter("id"));
+		wallet w = wallets.findByAccountId(id);
+		return w.getAmount();
 	}
 }
--- src/main/java/com/betvn/aptech88/repository/paymentRepository.java
+++ src/main/java/com/betvn/aptech88/repository/paymentRepository.java
@@ -2,13 +2,22 @@ package com.betvn.aptech88.repository;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.betvn.aptech88.model.payment;
 
 
-@Repository
 public interface paymentRepository extends JpaRepository<payment, Integer> {
 	List<payment> findAllByWalletId( int walletId);
+
+	@Query(value="select * from payment where wallet_id = ?1 order by(id) desc limit ?2, ?3",nativeQuery = true)
+	List<Object[]> findPaymentPagination(int walletId, int skip, int take);
+
+	@Query(value="select * from payment where payment_date >= ?1 AND payment_date <= ?2 AND wallet_id = ?5 order by(id) desc limit ?3, ?4",nativeQuery = true)
+	List<Object[]> filterDate(String fromDate, String toDate, int skip, int take, int wallet_id);
+
+	@Query(value="select * from payment where payment_date >= ?1 AND payment_date <= ?2 AND wallet_id = ?3 order by(id) desc",nativeQuery = true)
+	List<Object[]> countOfFilter(String fromDate, String toDate, int wallet_id);
 }
